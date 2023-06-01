1cc528a7b234862b7cfb39dd9fcdc1931656d97b
dylanwheatley <dylan.r.wheatley@gmail.com>
2023-06-01 08:11:49 -0400

    added initial pseudo

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+//store gameboard inside of an array, inside of an gameboard object. Module?
+
+//players inside of objects
+
+//object to control game
+
+
+//tip - one of something is a module, multiples (players) is a factory
+
+
+
